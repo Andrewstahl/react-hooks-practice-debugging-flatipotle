c0c0d915da993707f071741136d54c1693f60b4a
Andrew Stahl <andrewstahl96@gmail.com>
2022-07-19 21:09:40 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,20 +11,21 @@ const DEFAULT_STATE = {
   sides: [],
 };
 
-function Form() {
+function Form({ addOrder }) {
   const [formState, setFormState] = useState(DEFAULT_STATE);
 
-  function handleSubmit() {
+  function handleSubmit(event) {
     event.preventDefault();
-    props.addOrder(formState);
-
+    
     setFormState({
       ...DEFAULT_STATE,
     });
+    
+    addOrder(formState);
     event.target.reset();
   }
 
-  function handleChange() {
+  function handleChange(event) {
     const itemType = event.target.name;
     const item = event.target.value;
 
